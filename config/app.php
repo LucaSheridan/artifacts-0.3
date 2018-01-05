0b7df55080220cb57ae961ef887210b2ff7525f6
LucaSheridan <lucas_sheridan@me.com>
2018-01-05 14:42:37 -0500

    Update app.php

--- config/app.php
+++ config/app.php
@@ -165,7 +165,6 @@ return [
         Collective\Html\HtmlServiceProvider::class,
         Laracasts\Flash\FlashServiceProvider::class,
         Intervention\Image\ImageServiceProvider::class,
-        //Barryvdh\Debugbar\ServiceProvider::class,
         Spatie\Backup\BackupServiceProvider::class,
 
         /*
@@ -233,7 +232,6 @@ return [
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
         'Image' => Intervention\Image\Facades\Image::class,
-        //'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
     ],
 
